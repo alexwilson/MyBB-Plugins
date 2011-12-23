20fbf348ea27569f7abd881a612cd4288d0b110f
Alex "Antoligy" Wilson <antoligy@antoligy.com>
2011-12-23 16:41:04 +0000

    Massive improvements to Cloudflare Management: * Now stores settings in the database. * Implemented proper install/uninstall procedure. * Cleaned up code somewhat. * Automatic Zone ID look-ups.

--- Cloudflare Management/Cloudflare.php
+++ Cloudflare Management/Cloudflare.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Antoligy MyBB Cloudflare Management Plugin - See README.md
+Antoligy MyBB Cloudflare Management Plugin v1.2
 
 Copyright (c) 2011, Alex "Antoligy" Wilson <antoligy@antoligy.com>
 
 Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
+purpose without fee is hereby granted, provided that the above copyright
+notice and this permission notice appear in all copies.
 
 THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
@@ -17,36 +17,20 @@ ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
-// Config (needs migration to the database)
-$Cloudflare = array(
-			'API' => 'https://www.cloudflare.com/api_json.html',
-			'API_Key' => '<INSERT API KEY HERE>',
-			'Email' => '<INSERT@EMAIL.HERE>',
-			'Domain' => '<INSERT-DOMAIN.HERE>',
-			'Timeout' => 10,
-			'Zone' => <INSERT-ZONE-HERE>,
-			'Data' => array(),
-			);
-
 // Mandidatory Security Check
-if(!defined("IN_MYBB")) {
-        die("Go away.");
+if(!defined('IN_MYBB')) {
+        die('Go away.');
 }
 
-$plugins->add_hook("admin_tools_menu", "Cloudflare_menu");
-$plugins->add_hook("admin_load", "Cloudflare_admin");
-$plugins->add_hook("admin_tools_action_handler", "Cloudflare_action_handler");
-
-if(isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
-  $mybb->ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
-}
+$plugins->add_hook('admin_tools_menu', 'Cloudflare_menu');
+$plugins->add_hook('admin_load', 'Cloudflare_admin');
+$plugins->add_hook('admin_tools_action_handler', 'Cloudflare_action_handler');
 
 function Cloudflare_menu(&$sub_menu) {
     global $mybb;
     end($sub_menu);
     $key = (key($sub_menu))+10;
-    if(!$key)
-    {
+    if(!$key) {
         $key = '160';
     }
     $sub_menu[$key] = array('id' => 'Cloudflare', 'title' => 'Cloudflare Management', 'link' => "index.php?module=tools-Cloudflare");
@@ -54,13 +38,12 @@ function Cloudflare_menu(&$sub_menu) {
 
 function Cloudflare_action_handler(&$action) {
 	$action['Cloudflare'] = array('active' => 'Cloudflare', 'file' => '../../../inc/plugins/Cloudflare.php');
-//	$action['Cloudflare'] = array('active' => 'Cloudflare', 'file' => MYBB_ROOT./inc/plugins/Cloudflare.php'); // WHY CAN'T I INCLUDE THIS :\
+//	$action['Cloudflare'] = array('active' => 'Cloudflare', 'file' => MYBB_ROOT./inc/plugins/Cloudflare.php'); #WHY CAN'T I INCLUDE THIS :\
 }
 
 function Cloudflare_admin() {
 	global $page, $mybb;
-	if($page->active_action != "Cloudflare")
-	{
+	if($page->active_action != "Cloudflare") {
 		return;
 	}
 	$page->add_breadcrumb_item("Cloudflare Management");
@@ -68,15 +51,87 @@ function Cloudflare_admin() {
 
 function Cloudflare_info() {
 	return array(
-		"name" => "Cloudflare Management",
-		"description" => "Basic remote administration for a Cloudflare controlled domain",
-		"website" => "http://antoligy.com/",
-		"author" => "Alex \"Antoligy\" Wilson",
-		"authorsite" => "http://antoligy.com/",
-		"version" => "1.0",
-		"guid" => "18bafdcd24f80ba02aa2cd69fcd4f296",
-		"combatibility" => "*"
+		'name' => 'Cloudflare Management',
+		'description' => 'Basic remote administration for a Cloudflare controlled domain',
+		'website' => 'http://antoligy.com/',
+		'author' => 'Alex "Antoligy" Wilson',
+		'authorsite' => 'http://antoligy.com/',
+		'version' => '1.2',
+		'guid' => '18bafdcd24f80ba02aa2cd69fcd4f296',
+		'combatibility' => '*',
 	);
+}
+
+function Cloudflare_install() {
+	global $db, $mybb;
+	$db->query('DELETE FROM ' . TABLE_PREFIX . "settinggroups WHERE name='Cloudflare'");
+	$db->query('DELETE FROM ' . TABLE_PREFIX . "settings WHERE name LIKE 'Cloudflare_%'");
+	$db->query('UPDATE  ' . TABLE_PREFIX . "settings set value='1' WHERE name='ip_forwarded_check'");
+	$db->insert_query('settinggroups', array('gid' => 'NULL',
+																					 'name' => 'Cloudflare',
+																					 'title' => 'Cloudflare Management',
+																					 'description' => 'Configure your Cloudflare settings.',
+																					 'disporder' => 1,
+																					 'isdefault' => 0,
+																					 )
+									 );
+  $gid = intval($db->insert_id());
+	$settings = array('Cloudflare_Domain' => array(
+																								'title' => 'Domain',
+																								'description' => 'Domain of Cloudflare managed website (probably this one).',
+																								'optionscode' => 'text',
+																								'value' => $mybb->settings['cookiedomain'],
+																								'disporder' => 1,
+																								),
+									 'Cloudflare_Email' => array('title' => 'Email',
+																								'description' => 'The email address you use with your Cloudflare account.',
+																								'optionscode' => 'text',
+																								'value' => $mybb->user['email'],
+																								'disporder' => 2,
+																								),
+									 'Cloudflare_API_Key' => array('title' => 'API Key',
+									 															'description' => 'Your CloudFlare API key (<a href="https://www.cloudflare.com/my-account">Click here to find your API key</a>).',
+									 															'optionscode' => 'text',
+									 															'value' => '63example612a5545144347863bb09192919example57',
+									 															'disporder' => 3,
+									 															),
+								 );
+	foreach($settings as $name => $setting) {
+		$setting['gid'] = $gid;
+		$setting['name'] = $name;
+		$db->insert_query('settings', $setting);
+	}
+	unset($default);
+	rebuild_settings();
+}
+
+$Cloudflare = array(
+			'API' => 'https://www.cloudflare.com/api_json.html',
+			'API_Key' => $mybb->settings['Cloudflare_API_Key'],
+			'Email' => $mybb->settings['Cloudflare_Email'],
+			'Domain' => $mybb->settings['Cloudflare_Domain'],
+			'Timeout' => 10,
+			'Zone' => $mybb->settings['Cloudflare_Zone'],
+			'Data' => array(),
+			'Query' => array(),
+);
+
+function Cloudflare_is_installed() {
+	global $db;
+	$Cloudflare['Query'] = $db->query('SELECT * FROM ' . TABLE_PREFIX . "settinggroups WHERE name='Cloudflare'");
+ 	if($db->num_rows($Cloudflare['Query']) > 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+} 
+
+function Cloudflare_uninstall() {
+	global $db;
+	$db->query('DELETE FROM ' . TABLE_PREFIX . "settinggroups WHERE name='Cloudflare'");
+	$db->query('DELETE FROM ' . TABLE_PREFIX . "settings WHERE name LIKE 'Cloudflare_%'");
+	rebuild_settings();
 }
 
 function unidate($date) {
@@ -116,13 +171,12 @@ function Cloudflare_POST() {
 	}
 	else {
 		unset($c);
-//		print_r($Cloudflare['data']);
 		header('Location: index.php?module=tools-Cloudflare');
 	}
 }
 
 function Cloudflare_Form() {
-	global $Cloudflare;
+	global $Cloudflare, $db;
 	switch($_GET['cfaction']) {
 		case 'devmode':
 			if(isset($_GET['devmode'])) {
@@ -185,6 +239,12 @@ function Cloudflare_Form() {
 	      		);
          		Cloudflare_POST($Cloudflare['data']);
 					break;
+					
+/*					case 'resetzone':
+						$db->query('DELETE FROM ' . TABLE_PREFIX . "settings WHERE name='Cloudflare_Zone'");
+						header('Location: index.php?module=tools-Cloudflare');
+					break;
+*/
 
 				}
 			}
@@ -223,9 +283,17 @@ function Cloudflare_Main() {
 
 	$output = array();
 
-//	switch($_GET['action']) {
-//		case 'stats':
-
+/*	
+		$output['core'] = new Table;
+		$output['core']->construct_header('<b>Type</b>', array("width" => 180));
+		$output['core']->construct_header('<b>Value</b>');
+		$output['core']->construct_row();
+		$output['core']->construct_cell('<b>Reset Zone ID:</b>');
+		$output['core']->construct_cell('<form method="get"><input type="hidden" name="module" value="tools-Cloudflare"><input type="hidden" name="cfaction" value="resetzone"></select>&nbsp;<input type="submit" value="Apply" /></form>');
+		$output['core']->construct_row();
+		$output['core']->output('<b><u>Core Settings</b></u>');
+*/
+		
 		// Time
 		$output['time'] = new Table;
 		$output['time']->construct_header('<b>Type</b>', array("width" => 180));
@@ -375,31 +443,55 @@ function Cloudflare_Main() {
 		$output['bandwidth']->construct_row();
 
 		$output['bandwidth']->output('<b><u>Bandwidth</b></u>');
-
-//var_dump($Cloudflare);
-//		break;
-//	}
 }
+
 if($mybb->input['module'] == 'tools-Cloudflare') {
+	if(!isset($mybb->settings['Cloudflare_Zone']) OR ($mybb->settings['Cloudflare_Zone'] == '')) {
+		$Cloudflare['Query'] = array(
+			'a' => 'zone_check',
+			'zones' => &$Cloudflare['Domain'],
+			'u' => &$Cloudflare['Email'],
+			'tkn' => &$Cloudflare['API_Key'],
+		);
+		$c = array();
+		$c['c'] = curl_init();
+		curl_setopt(&$c['c'], CURLOPT_VERBOSE, 0);
+    curl_setopt(&$c['c'], CURLOPT_FORBID_REUSE, true);
+    curl_setopt(&$c['c'], CURLOPT_URL, &$Cloudflare['API']);
+    curl_setopt(&$c['c'], CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt(&$c['c'], CURLOPT_POST, 1);
+    curl_setopt(&$c['c'], CURLOPT_POSTFIELDS, $Cloudflare['Query']);
+    curl_setopt(&$c['c'], CURLOPT_TIMEOUT, &$Cloudflare['Timeout']);
+  	$c['crs'] = curl_exec(&$c['c']);
+		$c['cer'] = curl_error(&$c['c']);
+		$c['cht'] = curl_getinfo(&$c['c'], CURLINFO_HTTP_CODE);
+		curl_close($c['c']);
+		if($c['cht'] != 200) {
+			die('Cloudflare API Error: ' . $c['cer'] . "\n");
+		}
+		else {
+			$Cloudflare['Data'] = json_decode(&$c['crs'], true);
+			unset($c);
+			//die(print_r($Cloudflare));
+			$gid = $db->fetch_field($db->simple_select('settings', '*', "name='Cloudflare_Domain'"),'gid');
+			$db->insert_query('settings', array('gid' => intval($gid), 'name' => 'Cloudflare_Zone', 'title' => 'Cloudflare Zone ID' , 'description' => 'The ID of your domains Zone that is used internally by Cloudflare. (Found <a href="https://www.cloudflare.com/api_json.html?a=zone_check&zones=' . $Cloudflare['Domain'] . '&u=' . $Cloudflare['Email'] . '&tkn=' . $Cloudflare['API_Key'] . '">here</a>).', 'optionscode' => 'text', 'value' => $Cloudflare['Data']['response']['zones'][$Cloudflare['Domain']], 'disporder' => 4));
+			rebuild_settings();
+			header('Location: index.php?module=tools-Cloudflare');
+		}
+	}
 	if(!is_super_admin($mybb->user['uid'])) {
 		flash_message($lang->cannot_perform_action_super_admin_general, 'error');
-		} else {
+	} else {
 		if(!$mybb->input['action']) {
 			$page->add_breadcrumb_item('Cloudflare Management', 'index.php?module=tools-Cloudflare');
 			$page->output_header();
 			print('<img src="http://www.cloudflare.com/images/layout/cloudflare-logo.png"><br />'."\n");
 			if(isset($_GET['cfaction'])) {
 				Cloudflare_Form($Cloudflare);
-//				Cloudflare_POST($Cloudflare);
-			}
+			} 
 			else {
 				Cloudflare_Main($Cloudflare);
 			}
-//		var_dump($Cloudflare);
-
-//		print_r($data);
-//		print('test');
-//		print_r($GLOBALS['data']);
 			unset($Cloudflare);
 			$page->output_footer();
 		}
